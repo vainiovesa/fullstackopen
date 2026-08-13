75d20dea574fae6c73450a647e6fa69bf5c470a0
vainiovesa <vesa.vainio@helsinki.fi>
2026-08-13 07:06:20 +0300

    Complete exercise 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,15 @@ const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const Statistics = (props) => {
   const [good, neutral, bad] = props.stats
+
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+
   const total = good + neutral + bad
   const average = (good - bad) / total || 0
   const positive = good / total || 0
