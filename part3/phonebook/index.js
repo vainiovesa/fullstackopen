fd4604d8281e4efb947e60e55255307465ede5b9
vainiovesa <vesa.vainio@helsinki.fi>
2026-08-19 10:39:22 +0300

    Complete exercise 3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -26,13 +26,25 @@ let persons = [
 ]
 
 
+app.get('/info', (request, response) => {
+  response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${Date()}</p>`)
+})
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
-app.get('/info', (request, response) => {
-  response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${Date()}</p>`)
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(note => note.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
 })
+
 
 const PORT = 3001
 app.listen(PORT, () => {
