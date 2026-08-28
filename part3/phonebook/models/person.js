565eeed8dd2f0f9e06e66d2d89bf99abe3e31876
vainiovesa <vesa.vainio@helsinki.fi>
2026-08-28 14:40:28 +0300

    Complete exercise 3.20*

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -21,7 +21,16 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true
   },
-  number: String,
+  number: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /(?=\d{2,3}-\d{4,})(?=[\d-]{8,})/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
+  }
 })
 
 personSchema.set('toJSON', {
