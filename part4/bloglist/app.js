d2b9b584b72c07045de1563bc60f4a6ce6993c4d
vainiovesa <vesa.vainio@helsinki.fi>
2026-08-31 18:35:33 +0300

    Complete exercise 4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogRouter = require('./controllers/blogs')
+
+const app = express()
+
+mongoose.connect(config.MONGODB_URI, { family: 4 })
+
+app.use(express.json())
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
