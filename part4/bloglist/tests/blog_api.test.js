bb73cdb5ae41e00325b4efb70089b512589709da
vainiovesa <vesa.vainio@helsinki.fi>
2026-09-01 14:02:12 +0300

    Complete exercise 4.10

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -36,6 +36,27 @@ test('id field called id', async () => {
   })
 })
 
+test('a valid blog can be added ', async () => {
+  const newBlog = {
+    title: "valid",
+    author: "Tester",
+    url: "https://example.com/",
+    likes: 1,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+  const contents = blogsAtEnd.map(b => b.title)
+  assert(contents.includes('valid'))
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
