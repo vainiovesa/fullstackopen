9050ef82fe666ebf54c8529f04bc1c8a3128c9f4
vainiovesa <vesa.vainio@helsinki.fi>
2026-09-01 12:45:25 +0300

    Complete exercise 4.8

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,32 @@
+const assert = require('node:assert')
+const { test, after, beforeEach } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('Blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
