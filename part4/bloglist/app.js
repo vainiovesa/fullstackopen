d4008bf7ba739d79899fb86baba647e054b8f9f0
vainiovesa <vesa.vainio@helsinki.fi>
2026-09-07 19:07:15 +0300

    Complete exercise 4.18

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,6 +4,7 @@ const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 const blogRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -14,6 +15,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
