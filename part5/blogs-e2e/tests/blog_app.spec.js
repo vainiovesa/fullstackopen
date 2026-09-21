0fa80b11e42383b88ed1adb21c1cbae6d67e297c
vainiovesa <vesa.vainio@helsinki.fi>
2026-09-21 21:17:11 +0300

    Complete exercise 5.17

---
+++ part5/blogs-e2e/tests/blog_app.spec.js
@@ -0,0 +1,20 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Esther Tester',
+        username: 'tester',
+        password: 'sekret'
+      }
+    })
+
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByText('login')).toBeVisible()
+  })
+})
