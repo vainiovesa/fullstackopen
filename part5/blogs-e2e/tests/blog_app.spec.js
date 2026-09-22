073f87a2f8b7cc7d503c0790586042c2d9a0c417
vainiovesa <vesa.vainio@helsinki.fi>
2026-09-22 09:53:30 +0300

    Complete exercise 5.18

--- part5/blogs-e2e/tests/blog_app.spec.js
+++ part5/blogs-e2e/tests/blog_app.spec.js
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith } = require('./helper')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -16,5 +17,19 @@ describe('Blog app', () => {
 
   test('Login form is shown', async ({ page }) => {
     await expect(page.getByText('login')).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'tester', 'sekret')
+      await expect(page.getByText('Esther Tester logged in')).toBeVisible()
+      await expect(page.getByText('wrong username or password')).not.toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'tester', 'wrong')
+      await expect(page.getByText('wrong username or password')).toBeVisible()
+      await expect(page.getByText('Esther Tester logged in')).not.toBeVisible()
+    })
   })
 })
