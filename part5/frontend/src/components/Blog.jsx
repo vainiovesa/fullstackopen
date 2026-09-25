0e9664491c3f7a16a120108b466d3d89d90d3a2a
vainiovesa <vesa.vainio@helsinki.fi>
2026-09-25 18:38:37 +0300

    Complete exercise 5.31

--- part5/frontend/src/components/Blog.jsx
+++ part5/frontend/src/components/Blog.jsx
@@ -1,3 +1,23 @@
+import styled from 'styled-components'
+
+const BlogElement = styled.div`
+  margin: 1em;
+  padding: 0.5em 1em;
+  border: none;
+  border-radius: 10px;
+  border: 1px solid black;
+`
+
+const Button = styled.button`
+  background: White;
+  text-transform: uppercase;
+  font-size: 1.2em;
+  padding: 2px 10px;
+  border: 2px solid Dodgerblue;
+  border-radius: 10px;
+  cursor: pointer;
+`
+
 const Blog = ({ blog, handleLike, handleRemove, user }) => {
 
   const like = () => {
@@ -9,25 +29,25 @@ const Blog = ({ blog, handleLike, handleRemove, user }) => {
   }
 
   return (
-    <div>
+    <BlogElement>
       <div>
         <h3>
           {blog.author}: {blog.title}
         </h3>
         <a href={blog.url}>{blog.url}</a>
         <div>
-          likes {blog.likes} {user && <button onClick={like}>like</button>}
+          likes {blog.likes} {user && <Button onClick={like}>like</Button>}
         </div>
         <div>
           Added by {blog.user.username}
         </div>
         {user && user.username === blog.user.username &&
           <div>
-            <button onClick={remove}>remove</button>
+            <Button onClick={remove}>remove</Button>
           </div>
         }
       </div>
-    </div>
+    </BlogElement>
   )}
 
 export default Blog
